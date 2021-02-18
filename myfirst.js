11ef7ec5597e9e5fe3a1ef0cd6e3fce7707b4cdb
kevinnls <57634663+kevinnls@users.noreply.github.com>
2021-02-18 16:43:02 +0530

    minor improvements

--- myfirst.js
+++ myfirst.js
@@ -3,6 +3,8 @@ var mod = require('./myDateModule')
 
 http.createServer(function(req, res){
     res.writeHead(200, {'content-type':'text/html'})
-    res.write('Hello World!')
+    res.write('Hello World!\n')
     res.end("The date and time are currently: " + mod.myDateTime());
 }).listen(8080)
+console.log("the server is listening on port 8080");
+console.log("http://localhost:8080");
